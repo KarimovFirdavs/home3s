a13d9074ab233f888f53ac176ca72f6822692dd8
KarimovFirdavsFE[14] <115251742+KarimovFirdavs@users.noreply.github.com>
2022-10-22 18:08:02 +0500

    Add files via upload

---
+++ New homework JS/js/script.js
@@ -0,0 +1,25 @@
+
+
+// 1 uyda vazifa
+
+// let arr = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',]
+// for (let i = 1; i <= arr.length; i++) {
+//     console.log(i + " " + arr[i]);
+// }
+
+// 2 uyda vazifa
+
+let arrOne = ['a', 'b', 'c', 'd', 'i', 'f', 'g'];
+function Push(alifbo) {
+    arrOne[length] = alifbo;
+    console.log(arrOne)
+}
+Push('s');
+
+function popArr() {
+    arrOne.splice(arrOne.length - 1, 1);
+    console.log(arrOne);
+}
+popArr()
+
+
